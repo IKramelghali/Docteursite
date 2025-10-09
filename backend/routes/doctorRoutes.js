cb09a95fb0a51ecfc4948a71a9baef54afcbab04
elghali-ikram <ikram.elghali.solicode@gmail.com>
2025-10-09 14:02:30 +0100

    change

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {getDoctors,getDoctorById,updateDoctor,deleteDoctor} = require("../controllers/doctorController"); 
 const upload = require("../middleware/multer");
+const {authMiddleware}=require("../middleware/authMiddleware")
 
 // Routes
 
 router.get("/", getDoctors);          
 router.get("/:id", getDoctorById);    
-router.put("/:id", upload.single("profile"), updateDoctor);     
-router.delete("/:id", deleteDoctor);   
+router.put("/:id",authMiddleware, upload.single("profile"), updateDoctor);     
+router.delete("/:id",authMiddleware, deleteDoctor);   
 
 module.exports = router;
